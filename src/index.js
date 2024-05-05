26b70e4a4cb0b1386c86744fadae3cee1fa426ff
Koffi <124451886+Koffi-exe@users.noreply.github.com>
2024-05-05 20:59:28 +0530

    CORS policy updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
 import { connectdb } from './db/connection.js';
-import MensRanking from './models/mens100.models.js';
 import raceRouter from './routes/mens.routes.js';
 import auth1Router from './routes/auth1.routes.js';
 
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res)=>res.send("This is the Merged adithya api"))
